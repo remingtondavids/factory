6c9e1e4562ac297eb45710addf3b9b45581babf4
remingtondavids <53348044+remingtondavids@users.noreply.github.com>
2022-04-06 23:19:11 -0400

    Initial commit

---
+++ factory.js
@@ -0,0 +1,38 @@
+class Factory {
+
+    constructor(size, money) {
+
+        this.size = size;
+        this.money = money;
+    }
+
+    size;
+    money;
+
+    buildMiner() {
+
+        
+
+    }
+
+
+
+
+
+}
+
+class Miner {
+
+}
+
+class Ore {
+
+}
+
+class Storage {
+
+}
+
+class Transporter {
+
+}
